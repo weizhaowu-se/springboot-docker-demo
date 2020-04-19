d0bdfa177ee45aea5fca55fa57cce1472737fe97
weizhaowu@outlook.com <weizhaowu@outlook.com>
2020-04-19 16:06:43 +0800

    init

---
+++ src/main/java/com/wilbert/dockerdomer/DockerDomerApplication.java
@@ -0,0 +1,13 @@
+package com.wilbert.dockerdomer;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DockerDomerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DockerDomerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/wilbert/dockerdomer/DockerDomerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.wilbert.dockerdomer;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DockerDomerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
